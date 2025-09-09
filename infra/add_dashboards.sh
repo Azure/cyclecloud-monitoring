9dc6e37da555f80d96faaba5571b0eb97b5d679d
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-09-09 14:44:31 +0200

    don't create folder if already exists

--- infra/add_dashboards.sh
+++ infra/add_dashboards.sh
@@ -15,8 +15,13 @@ fi
 
 FOLDER_NAME="Azure CycleCloud"
 DASHBOARD_FOLDER=$THIS_DIR/dashboards
-# Create Grafana dashobards folders
-az grafana folder create --name $GRAFANA_NAME --resource-group $RESOURCE_GROUP_NAME --title "$FOLDER_NAME"
+# Create Grafana dashboards folders
+az grafana folder show -n $GRAFANA_NAME -g $RESOURCE_GROUP_NAME --folder "$FOLDER_NAME" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "$FOLDER_NAME folder does not exist. Creating it."
+  az grafana folder create --name $GRAFANA_NAME --resource-group $RESOURCE_GROUP_NAME --title "$FOLDER_NAME"
+fi
+
 
 # Single node dashboards
 az grafana dashboard import --name $GRAFANA_NAME --resource-group $RESOURCE_GROUP_NAME --folder "$FOLDER_NAME" --overwrite true --definition $DASHBOARD_FOLDER/node.json
